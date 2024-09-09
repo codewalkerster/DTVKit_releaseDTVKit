a6bde08db31d21236b1035cf2e84e90f3b5d9190
Haiting Feng <haiting.feng@amlogic.com>
2024-09-09 02:01:26 +0000

    releaseDTVKit: build error if not find the soc config [1/1]
    PD#SWPL-185219
    
    Problem:
    [DTVKIT][releaseDTVKit] use hw_config/config_default.xml if no Soc added
    
    Solution:
    use config-default.xml if not find the soc config
    
    Verify:
    manual test is ok
    
    Change-Id: Ibf9ba059117766c8d0187a1141174a07bd4cf913
    Signed-off-by: Haiting Feng <haiting.feng@amlogic.com>

--- config.mk
+++ config.mk
@@ -1,7 +1,11 @@
-DTVKIT_HWCONFIG_FILE := config-$(DTVKIT_HW_CONFIG_SOC).xml
+DTVKIT_HWCONFIG_FILE := vendor/amlogic/reference/external/DTVKit/releaseDTVKit/hw_config/config-$(DTVKIT_HW_CONFIG_SOC).xml
 
+ifeq ($(wildcard $(DTVKIT_HWCONFIG_FILE)),)
+$(error "$(DTVKIT_HWCONFIG_FILE) does not exist, please add config-$(DTVKIT_HW_CONFIG_SOC).xml")
+else
 $(warning "Copy dtvkit hw_config/$(DTVKIT_HWCONFIG_FILE) to $(TARGET_COPY_OUT_VENDOR)/etc/dtvkit/config.xml")
-PRODUCT_COPY_FILES += vendor/amlogic/reference/external/DTVKit/releaseDTVKit/hw_config/$(DTVKIT_HWCONFIG_FILE):$(TARGET_COPY_OUT_VENDOR)/etc/dtvkit/config.xml
+PRODUCT_COPY_FILES += $(DTVKIT_HWCONFIG_FILE):$(TARGET_COPY_OUT_VENDOR)/etc/dtvkit/config.xml
+endif
 
 $(warning "Copy dtvkit sw_config to $(TARGET_COPY_OUT_VENDOR)/etc/dtvkit")
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/amlogic/reference/external/DTVKit/releaseDTVKit/sw_config,$(TARGET_COPY_OUT_VENDOR)/etc/dtvkit/)
